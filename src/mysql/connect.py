805513ffd31ad9de708f5aa93b517714f9981210
chuyouhong <chuyh@medishare.cn>
2018-02-07 16:56:41 +0800

    update

--- src/mysql/connect.py
+++ src/mysql/connect.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import MySQLdb as mdb
+import pymysql
 
 # 连接数据库
 #conn = mdb.connect('localhost', 'root', 'root')
 
 # 也可以使用关键字参数
-conn = mdb.connect(host='121.40.33.21', port=3306, user='root', passwd='123', db='new_test', charset='utf8')
+conn = pymysql.connect(host='121.40.33.21', port=3306, user='root', passwd='mixiguoji123', db='new_test', charset='utf8')
 
 # 也可以使用字典进行连接参数的管理
 # config = {
@@ -20,10 +20,10 @@ conn = mdb.connect(host='121.40.33.21', port=3306, user='root', passwd='123', db
 # conn = mdb.connect(**config)
 
 # 如果使用事务引擎，可以设置自动提交事务，或者在每次操作完成后手动提交事务conn.commit()
-conn.autocommit(1)    # conn.autocommit(True) 
+conn.autocommit(1)    # conn.autocommit(True)
 
 # 使用cursor()方法获取操作游标
-cursor = conn.cursor()
+cursor = conn.cursor(pymysql.cursors.DictCursor)
 # 因该模块底层其实是调用CAPI的，所以，需要先得到当前指向数据库的指针。
 
 try:
@@ -34,42 +34,43 @@ try:
 #     conn.select_db(DB_NAME)
 
     #创建表
-    TABLE_NAME = 'item'
+    TABLE_NAME = 'items'
     cursor.execute('CREATE TABLE %s(id int primary key,name varchar(30))' %TABLE_NAME)
 
     # 插入单条数据
-    sql = 'INSERT INTO item values("%d","%s")' %(1,"jack")
+    sql = 'INSERT INTO items values("%d","%s")' %(1,"jack")
 
     # 不建议直接拼接sql，占位符方面可能会出问题，execute提供了直接传值
     value = [2,'John']
-    cursor.execute('INSERT INTO item values(%s,%s)',value)
+    cursor.execute('INSERT INTO items values(%s,%s)',value)
 
     # 批量插入数据
     values = []
     for i in range(3, 20):
-        values.append((i,'kk'+str(i)))
-    cursor.executemany('INSERT INTO item values(%s,%s)',values)
+        values.append((i,'kkhff'+str(i)))
+    cursor.executemany('INSERT INTO items values(%s,%s)',values)
 
     # 查询数据条目
     count = cursor.execute('SELECT * FROM %s' %TABLE_NAME)
-    print 'total records: %d' %count
-    print 'total records:', cursor.rowcount
+    print('total records: %d' %count)
+    print ('total records:', cursor.rowcount)
 
     # 获取表名信息
     desc = cursor.description
-    print "%s %3s" % (desc[0][0], desc[1][0])
+    print ("%s %3s" % (desc[0][0], desc[1][0]))
 
     # 查询一条记录
-    print 'fetch one record:'
+    print ('fetch one record:')
     result = cursor.fetchone()
-    print result
-    print 'id: %s,name: %s' %(result[0],result[1])
+    print (result)
+    print (result["id"])
+    print( "id: %s,name: %s" %(result["id"],result["name"]))
 
     # 查询多条记录
-    print 'fetch five record:'
+    print( 'fetch five record:')
     results = cursor.fetchmany(5)
     for r in results:
-        print r
+        print (r)
 
     # 查询所有记录
     # 重置游标位置，偏移量:大于0向后移动;小于0向前移动，mode默认是relative
@@ -77,12 +78,12 @@ try:
     cursor.scroll(0,mode='absolute')
     results = cursor.fetchall()
     for r in results:
-        print r
+        print( r)
 
     cursor.scroll(-2)
     results = cursor.fetchall()
     for r in results:
-        print r
+        print (r)
 
     # 更新记录
     cursor.execute('UPDATE %s SET name = "%s" WHERE id = %s' %(TABLE_NAME,'Jack',1))
